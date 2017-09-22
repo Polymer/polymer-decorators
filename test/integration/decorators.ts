9ce3058a26289f887033a29e32f9ca7dda55fd5e
Alexander Marks <aomarks@google.com>
2017-09-21 17:24:22 -0700

    Fix WCT tests in Firefox and Safari.
    If your <test-fixture> has the same id as your <dom-module>, then when
    you don't have native HTML imports (where document is different) WCT's
    fixture('my-element') may return the <dom-module>, since it just does a
    getElementById.

--- test/integration/decorators.ts
+++ test/integration/decorators.ts
@@ -1,21 +1,25 @@
 /**
  * @license
  * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt The complete set of authors may be found
+ * at http://polymer.github.io/AUTHORS.txt The complete set of contributors may
+ * be found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by
+ * Google as part of the polymer project is also subject to an additional IP
+ * rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
 declare function fixture(id: string): HTMLElement;
 
 suite('TypeScript Decorators', function() {
+  let testElement: TestElement;
+
+  setup(function() {
+    testElement = fixture('test-element-fixture') as TestElement;
+  });
 
   suite('@customElement', function() {
-
     test('defines an element', function() {
-      var testElement = fixture('test-element');
       chai.assert.instanceOf(testElement, TestElement);
     });
 
@@ -24,10 +28,9 @@ suite('TypeScript Decorators', function() {
   suite('@property', function() {
 
     test('defines a property', function() {
-      var testElement = fixture('test-element') as TestElement;
       testElement.aNum = 999;
-      var numDiv = testElement.shadowRoot.querySelector('#num');
-      var numText = numDiv.textContent;
+      const numDiv = testElement.shadowRoot.querySelector('#num');
+      const numText = numDiv.textContent;
       chai.assert.equal(numText, '999');
     });
 
@@ -36,13 +39,11 @@ suite('TypeScript Decorators', function() {
   suite('@observe', function() {
 
     test('calls a method when a single observed property changes', function() {
-      var testElement = fixture('test-element') as TestElement;
       testElement.aNum = 999;
       chai.assert.equal(testElement.lastNumChange, 999);
     });
 
     test('calls a method when multiple observed properties change', function() {
-      var testElement = fixture('test-element') as TestElement;
       testElement.aNum = 999;
       chai.assert.equal(testElement.lastMultiChange[0], 999);
       testElement.aString = 'yahoo';
@@ -55,8 +56,7 @@ suite('TypeScript Decorators', function() {
   suite('@query', function() {
 
     test('queries the shadow root', function() {
-      var testElement = fixture('test-element') as TestElement;
-      var numDiv = testElement.numDiv;
+      const numDiv = testElement.numDiv;
       chai.assert.equal(numDiv.id, 'num');
     });
 
@@ -65,8 +65,7 @@ suite('TypeScript Decorators', function() {
   suite('@queryAll', function() {
 
     test('queries the shadow root', function() {
-      var testElement = fixture('test-element') as TestElement;
-      var divs = testElement.divs;
+      const divs = testElement.divs;
       chai.assert.equal(divs.length, 2);
     });
 
