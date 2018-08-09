a4ee6f963c9b06a03dafdca5f215d5ba38e9060f
Alexander Marks <aomarks@google.com>
2018-08-09 13:34:39 -0700

    Convert declarative-event-listeners to TypeScript.

--- src/declarative-event-listeners.ts
+++ src/declarative-event-listeners.ts
@@ -1,79 +1,81 @@
-<!--
-@license
-Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<link rel="import" href="../../polymer/lib/utils/boot.html">
-<link rel="import" href="../../polymer/lib/utils/mixin.html">
+/**
+ * @license
+ * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt The complete set of authors may be found
+ * at http://polymer.github.io/AUTHORS.txt The complete set of contributors may
+ * be found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by
+ * Google as part of the polymer project is also subject to an additional IP
+ * rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
-<script>
-  (function () {
-    'use strict';
-    /**
-    * @const {Polymer.DeclarativeEventListeners}
-    */
-    const declarativeListeners = Polymer.DeclarativeEventListeners;
-    /**
-    * Element class mixin that provides API for adding declarative event
-    * listeners nodes.
-    *
-    * The API is designed to be compatible with override points implemented
-    * in `Polymer.TemplateStamp` such that declarative event listeners in
-    * templates will support gesture events when this mixin is applied along with
-    * `Polymer.TemplateStamp`.
-    *
-    * @mixinFunction
-    * @polymer
-    * @memberof Polymer
-    * @summary Element class mixin that provides API for adding declarative event
-    * listeners nodes
-    */
-    Polymer.DeclarativeEventListeners = Polymer.dedupingMixin(superClass => {
+import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin.js';
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
 
+export interface SuperClassConstructor {
+  // tslint:disable-next-line:no-any Required for constructor signature.
+  new(...args: any[]): PolymerElement&{constructor: SuperClassConstructor};
+  listeners?: Array<{
+    target: string | Node,
+    eventName: string,
+    handler: (ev: Event) => void,
+  }>;
+}
+
+export interface DeclarativeEventListeners {
+  _addDeclarativeEventListener(
+      target: string|Node,
+      eventName: string,
+      handler: (ev: Event) => void): void;
+}
+
+/**
+ * Element class mixin that provides API for adding declarative event
+ * listener nodes.
+ *
+ * The API is designed to be compatible with override points implemented in
+ * `TemplateStamp` such that declarative event listeners in templates will
+ * support gesture events when this mixin is applied along with `TemplateStamp`.
+ */
+export const DeclarativeEventListeners = dedupingMixin(
+    <T extends SuperClassConstructor>(superClass: T): T&
+    DeclarativeEventListeners => {
       /**
-      * @polymer
-      * @mixinClass
-      * @implements {Polymer_DeclarativeEventListeners}
-      * @property listeners {object} Stores the declared event listeners to be subscribed to 
-      * durning ready().
-      */
-      class DeclarativeEventListeners extends superClass {
-
+       * @property listeners {object} Stores the declared event listeners to be
+       * subscribed to durning ready().
+       */
+      return class extends superClass {
         ready() {
           super.ready();
           if (!this.constructor.hasOwnProperty('listeners')) {
             this.constructor.listeners = [];
           }
 
-          this.constructor.listeners.forEach(listener => {
-            const targetElement = typeof listener.target === 'string' ? this.$[listener.target] : listener.target;
-            this._addEventListenerToNode(targetElement, listener.eventName, listener.handler.bind(this));
+          this.constructor.listeners!.forEach((listener) => {
+            const targetElement = typeof listener.target === 'string' ?
+                this.$[listener.target] :
+                listener.target;
+            this._addEventListenerToNode(
+                targetElement, listener.eventName, listener.handler.bind(this));
           });
         }
 
         /**
-        * Adds to stored listeners to subscribe to on ready() 
-        *
-        * @param {string|!Object} target Element/node for listener to target.
-        * @param {string} eventName Name of event to listen for.
-        * @param {function} handler Function which receives the notification.
-        * @return {void}
-        * @private
-        */
-        static _addDeclarativeEventListener(target, eventName, handler) {
+         * Adds to stored listeners to subscribe to on ready()
+         *
+         * @param target Element/node for listener to target.
+         * @param eventName Name of event to listen for.
+         * @param {function} handler Function which receives the notification.
+         */
+        static _addDeclarativeEventListener(
+            target: string|Node,
+            eventName: string,
+            handler: (ev: Event) => void): void {
           if (!this.hasOwnProperty('listeners')) {
             this.listeners = [];
           }
 
-          this.listeners.push({ target, eventName, handler });
+          this.listeners!.push({target, eventName, handler});
         }
-      }
-
-      return DeclarativeEventListeners;
+      };
     });
-  })();
-</script>
